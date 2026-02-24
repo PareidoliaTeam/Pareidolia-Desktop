0ddeb0727e62def72867f5535097a9e670e00d36
Rudy Cacho Hernandez <112025281+rudycacho@users.noreply.github.com>
2026-02-24 00:52:01 -0800

    Update extract_images.py

--- pareidolia-desktop/py/extract_images.py
+++ pareidolia-desktop/py/extract_images.py
@@ -1,6 +1,7 @@
 import cv2
 import sys
 import os
+from datetime import date
 
 def video_to_frames(video_path, output_folder):
     # create ouput folder if not already present
@@ -14,6 +15,8 @@ def video_to_frames(video_path, output_folder):
     save_interval = int (round(fps/2)) if fps > 0 else 15
     frame_count = 0
     saved_count = 0
+    video_name_f = os.path.basename(video_path)
+    video_name = os.path.splitext(video_name_f)[0]
     success = True
 
     while success:
@@ -23,7 +26,7 @@ def video_to_frames(video_path, output_folder):
         if success:
             if frame_count % save_interval == 0:
                 # create file name
-                file_name = f"frame_{frame_count}.jpg"
+                file_name = f"{video_name}_frame_{frame_count}.jpg"
                 file_path = os.path.join(output_folder, file_name)
             
                 # create jpeg
